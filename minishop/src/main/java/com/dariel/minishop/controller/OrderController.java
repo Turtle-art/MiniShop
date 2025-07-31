11d79f4d474bf73ba6604ec4129482d80cb588f4
Thulasizwe Sabela <thulasizwe.sabela@dariel.co.za>
2025-07-31 07:26:22 +0200

    Hide impl of payment service

--- minishop/src/main/java/com/dariel/minishop/controller/OrderController.java
+++ minishop/src/main/java/com/dariel/minishop/controller/OrderController.java
@@ -3,7 +3,7 @@ package com.dariel.minishop.controller;
 import com.dariel.minishop.dto.OrderDto;
 import com.dariel.minishop.mapper.OrderMapper;
 import com.dariel.minishop.model.Order;
-import com.dariel.minishop.service.impl.StripePaymentService;
+import com.dariel.minishop.service.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/orders")
 public class OrderController {
 
-    private final StripePaymentService stripePaymentService;
+    private final PaymentService paymentService;
     private final OrderMapper orderMapper;
 
-    public OrderController(StripePaymentService stripePaymentService, OrderMapper orderMapper) {
-        this.stripePaymentService = stripePaymentService;
+    public OrderController(PaymentService paymentService, OrderMapper orderMapper) {
+        this.paymentService = paymentService;
         this.orderMapper = orderMapper;
     }
 
     @PostMapping("/create-checkout-session")
     public ResponseEntity<String> createCheckoutSession(@RequestBody OrderDto orderDto) {
         Order order = orderMapper.mapTo(orderDto);
-        String sessionUrl = stripePaymentService.createCheckoutSession(order);
+        String sessionUrl = paymentService.createCheckoutSession(order);
         return ResponseEntity.ok(sessionUrl);
     }
 }
